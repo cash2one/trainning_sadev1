6f87c3c82c7e8c677cde300f0bdfc718d0942bb8
Abner Kang <Abner@Abners-MacBook-Pro.local>
2015-12-11 09:16:46 +0800

    for test

---
+++ code/agent/plugins/cpu.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- encoding:utf-8 -*-
+'''
+FileName: stage1_agent.py
+Author: Abner Kang
+@contact:gzkangjinglong@corp.netease.com
+@version:1.0
+Description:
+Changelog:
+'''
+from psutil import cpu_times_percent
+def cpu():
+    cpu_status = cpu_times_percent()
+    return {
+
+    }
+
+
