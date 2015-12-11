b5be83787124899177e310267b7b45011a90e80f
Abner Kang <gzkangjinglong@corp.netease.com>
2015-12-11 10:39:18 +0800

    finish cpu monitor

--- code/agent/agent.py
+++ code/agent/agent.py
@@ -14,30 +14,61 @@ import os
 import sys
 import logging
 
+import plugins
+
 #命令行参数解析
-parse = optparse.OptionParser()
+def parse_args():
+    parse = optparse.OptionParser()
 
-parse.add_option('-v','--version',
-                 action = 'store_true',
-                 default = False,
-                 dest = 'version',
-                 help = 'show program number and exit.',
-                 )
+    parse.add_option('-v','--version',
+                     action = 'store_true',
+                    default = False,
+                    dest = 'version',
+                    help = 'show program number and exit.',
+                    )
 
-parse.add_option('-t','--ttl',
-                 action = 'store',
-                 dest = 'ttl',
-                 default = '60',
-                 help = 'set agent period,default is 60s',
-                 )
-parse.add_option('-m','--module',
-                 action = 'store',
-                 dest = 'module',
-                 default = 'all',
-                 help = "set module MODULE ,default output all modules.\n \
-                 Avaliable Modules:"
-                 )
+    parse.add_option('-t','--ttl',
+                     action = 'store',
+                     dest = 'ttl',
+                     default = '60',
+                     help = 'set agent period,default is 60s',
+                     )
+    parse.add_option('-m','--module',
+                     action = 'store',
+                     dest = 'module',
+                     default = 'all',
+                     help = "set module MODULE ,default output all modules.\n \
+                    Avaliable Modules:cpu,memory,load"
+                    )
+    return parse
 
+if __name__ == '__main__':
+    from pprint import pprint
 
-options,remainder = parse.parse_args(sys.argv[1:])
-print options
+    options,remainder = parse_args().parse_args(sys.argv[1:])
+
+    cpu_monitor = plugins.cpu.monitor
+   #  load_monitor = plugins.load.monitor
+    # memory_monitor = plugins.memory.monitor
+
+    monitors = {'cpu':cpu_monitor,}
+
+    if options.version:
+        print 'Agent:1.0'
+        sys.exit(0)
+
+    if options.ttl:
+        try:
+            int(options.ttl)
+        except ValueError:
+            logging.error('the value of ttl must be a postive integer!!!')
+            exit(-1)
+
+    if options.module not in ['cpu','memory','load','all']:
+        logging.warning('Invalid module,avaliable modules are cpu,memory,load,all')
+        exit(-1)
+    elif options.module == 'cpu':
+        pprint(monitors['cpu']())
+    else:
+        pass
+
--- code/agent/plugins/cpu.py
+++ code/agent/plugins/cpu.py
@@ -9,10 +9,39 @@ Description:
 Changelog:
 '''
 from psutil import cpu_times_percent
-def cpu():
-    cpu_status = cpu_times_percent()
-    return {
 
+def monitor():
+     cpu_monitor = cpu_times_percent()
+     return {
+        #前三个属性所有平台都有
+        'user':cpu_monitor.user,
+        'system':cpu_monitor.system,
+        'idle':cpu_monitor.idle,
+
+        #可能会有些平台不提供下面的属性
+        'nice':cpu_monitor.nice \
+        if hasattr(cpu_monitor,'nice') else '',
+
+        'iowait':cpu_monitor.iowait\
+        if hasattr(cpu_monitor,'iowait') else '',
+
+        'irq':cpu_monitor.irq \
+        if hasattr(cpu_monitor,'irq') else '',
+
+        'softirq':cpu_monitor.softirq  \
+        if hasattr(cpu_monitor,'softirq') else '',
+
+        'steal':cpu_monitor.steal \
+        if hasattr(cpu_monitor,'steal') else '',
+
+        'guest':cpu_monitor.guest \
+        if hasattr(cpu_monitor,'guest') else '',
+
+        'guest_nice':cpu_monitor.guest_nice \
+        if hasattr(cpu_monitor,'guest_nice') else '',
     }
 
+
+if __name__ == '__main__':
+    print monitor()
 
